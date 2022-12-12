1eb198d8bc939b31cf5c56ffb13e5e7624e3a710
Justin Shilling <loveofmusic25@hotmail.com>
2022-12-11 23:23:07 -0500

    migrate productReviews store to pinia

---
+++ src/stores/productReviews.ts
@@ -0,0 +1,72 @@
+import { defineStore } from "pinia";
+import { productReviewRepository } from "@/repositories/productReviewRepository";
+
+export const useProductReviewsStore = defineStore("productReviews", {
+  state: () => {
+    return {
+      productReviews: [] as any,
+    };
+  },
+
+  actions: {
+    async updateProductReviews(payload: any) {
+      const { data } = await productReviewRepository.getAll(payload);
+      if (!data) return;
+
+      if (data.marketingV1GetProductReview.length > 0) {
+        this.productReviews = data.marketingV1GetProductReview;
+      }
+    },
+
+    async updateProductReview(payload: any) {
+      const { data } = await productReviewRepository.get(payload);
+      if (!data) return;
+
+      if (data.marketingV1GetProductReview.length > 0) {
+        const reviewIndex = this.productReviews.findIndex(
+          (product: any) => product.id == payload.productId
+        );
+        const { review } = payload;
+        if (reviewIndex >= 0) {
+          this.productReviews.splice(reviewIndex, 1, review);
+        } else {
+          this.productReviews.push(review);
+        }
+      }
+    },
+
+    async createProductReview(payload: any) {
+      const { data } = await productReviewRepository.post(payload);
+      if (!data) return;
+
+      const reviewIndex = this.productReviews.findIndex(
+        (review: any) => review.product.id == payload.productId
+      );
+      if (reviewIndex >= 0) {
+        this.productReviews.splice(reviewIndex, 1, data.review);
+      } else {
+        this.productReviews.push(data.review);
+      }
+    },
+
+    async deleteProductReview(payload: any) {
+      const { data } = await productReviewRepository.delete(payload);
+      if (!data) return;
+
+      const reviewIndex = this.productReviews.findIndex(
+        (review: any) =>
+          review.product.id == payload.productId &&
+          review.employee.employeeNumber == payload.employeeNumber
+      );
+      this.productReviews.splice(reviewIndex, 1);
+      // Vue.toasted.success("Review successfully deleted.");
+    },
+  },
+
+  getters: {
+    productReviewsByProductId: (state) => (productId: any) =>
+      state.productReviews.filter(
+        (review: any) => review.product.id == productId
+      ),
+  },
+});
