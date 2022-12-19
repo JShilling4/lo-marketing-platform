a90a486b2c7e8587ab8ca533775bce7ecfb99552
Justin Shilling <loveofmusic25@hotmail.com>
2022-12-18 22:54:18 -0500

    file structure changes

--- src/repositories/clients/index.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const graphql = axios.create({
-  baseURL: "",
-});
-
-export const rest = axios.create({
-  baseURL: "",
-});
--- src/types/repositories/productRepository.ts
+++
@@ -1,9 +0,0 @@
-import type { AxiosResponse } from "axios";
-import type { IProduct } from "../product";
-
-export interface IProductRepository {
-  getAll(): Promise<AxiosResponse<any, any>>;
-  get(id: string): Promise<AxiosResponse<any, any>>;
-  post(data: IProduct): Promise<AxiosResponse<any, any>>;
-  bulkPost(data: IProduct[]): Promise<AxiosResponse<any, any>>;
-}
